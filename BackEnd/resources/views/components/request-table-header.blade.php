ef11ba0b02233cccf9883d95db333bb0ee320aab
Lithashl <90357502+Lithashl@users.noreply.github.com>
2025-07-25 02:23:20 +0700

    Create request-table-header.blade.php

---
+++ BackEnd/resources/views/components/request-table-header.blade.php
@@ -0,0 +1,9 @@
+<div class="c_home-frame66">
+    @foreach ($headers as $header)
+        <div class="c_home-frame67">
+            <div class="c_home-text196">
+                <p class="c_home-text197">{{ $header }}</p>
+            </div>
+        </div>
+    @endforeach
+</div>
